cae99d755438f5de74e025e4ccea478c43f54d90
sayan mondal <sayanm816@gmail.com>
2019-07-15 21:12:20 +0530

    local html file added

---
+++ lib/deepscan.dart/load_html_file.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'dart:async';
+
+class LoadHTMLFileToWEbView extends StatefulWidget {
+  @override
+  _LoadHTMLFileToWEbViewState createState() => _LoadHTMLFileToWEbViewState();
+}
+
+class _LoadHTMLFileToWEbViewState extends State<LoadHTMLFileToWEbView> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: _loadLocalHTML(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return WebviewScaffold(
+            appBar: AppBar(title: Text("Load HTM file in WebView")),
+            withJavascript: true,
+            // appCacheEnabled: true,
+            url: new Uri.dataFromString(snapshot.data, mimeType: 'text/html')
+                .toString(),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("${snapshot.error}"),
+            ),
+          );
+        }
+        return Scaffold(
+          body: Center(child: CircularProgressIndicator()),
+        );
+      },
+    );
+  }
+}
+
+Future<String> _loadLocalHTML() async {
+  return await rootBundle.loadString('assets/web/medisol.github.io/index.html');
+}
--- lib/deepscan.dart/medi_web.dart
+++ lib/deepscan.dart/medi_web.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:medisol/deepscan.dart/load_html_file.dart';
 
-class DeepScan extends StatefulWidget {
-  static const String id = 'deepscan';
-  @override
-  _DeepScanState createState() => _DeepScanState();
-}
 
-class _DeepScanState extends State<DeepScan> {
+
+
+class DeepScan extends StatelessWidget {
+  static const String id  = 'deepscan';
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: AppBar(title: Text('deep scan'),),
-       body: WebviewScaffold(
-         url: 'https://suvhradipghosh07.github.io/medisol-web/',
-         
-         
-       ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadHTMLFileToWEbView(),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
